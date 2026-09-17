b62879ca765ca308682a7fd8f0bd90751486be6e
Jasonlee-kgms <wooddoll@koreaglobal.ms.kr>
2026-09-17 14:36:02 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -3,17 +3,17 @@
  */
 (function (g) {
   g.BUS_CONFIG = {
-    gasUrl: 'https://script.google.com/macros/s/여기에_배포ID/exec',
+    gasUrl: 'https://script.google.com/macros/s/AKfycbzPD2DcD-u_C6xc8g4N0yws1M8PfnD5Ra1-W6wPWFQ4sy3gSb6cJVF77DRuGdMs7mZkig/exec',
     firebase: {
-      apiKey: '',
-      authDomain: '',
-      projectId: '',
-      storageBucket: '',
-      messagingSenderId: '',
-      appId: ''
+      apiKey: 'AIzaSyBFLRnZUDefQ2hY3ewetd_hLAr0PgV2oS4',
+      authDomain: 'kgmsbus-1f649.firebaseapp.com',
+      projectId: 'kgmsbus-1f649',
+      storageBucket: 'kgmsbus-1f649.firebasestorage.app',
+      messagingSenderId: '429005922817',
+      appId: '1:429005922817:web:52cbd572149273790fd600'
     },
-    vapidKey: '',
-    googleClientId: '',
-    allowedDomain: ''
+    vapidKey: 'BLXqe9pUFJjlMCa-Xzr0Wh5LKsXGkI6tcXz5Y5Z6RG1o-DPHKBQBIkqScrHdRrcp2ExAUwdlbXu_3dbSUie2n88',
+    googleClientId: '866237942837-r1flmpg0urusn6ci1f6urgh2ii823d79.apps.googleusercontent.com',
+    allowedDomain: 'koreaglobal.ms.kr'
   };
 })(typeof self !== 'undefined' ? self : this);
